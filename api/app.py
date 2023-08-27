e1197bb6e0f42f3f6506c21a9214e283dfd851e9
Jamie Bernardi <jamie@Jamies-MacBook-Pro-2.local>
2023-08-27 23:35:04 +0100

    cleaning

--- api/app.py
+++ api/app.py
@@ -1,22 +1,11 @@
-from flask import Flask, Response
-from http.server import BaseHTTPRequestHandler
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return "Hello, World!"
+def home():
+    return 'Hello, World!'
 
-# def app_handler(environ, start_response):
-#     return app(environ, start_response)
-# 
-# class handler(BaseHTTPRequestHandler):
-#     def do_GET(self):
-#         self.send_response(200)
-#         self.send_header('Content-type','text/plain')
-#         self.end_headers()
-# 
-#         message = "Hello, World!"
-#         self.wfile.write(message.encode())
-#         return
-
+@app.route('/about')
+def about():
+    return 'About'
