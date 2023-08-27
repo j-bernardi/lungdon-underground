6660ad9197a91fd7eee24393deceef26c4162662
Jamie Bernardi <jamie@Jamies-MacBook-Pro-2.local>
2023-08-27 23:10:31 +0100

    making it simpler

--- api/app.py
+++ api/app.py
@@ -7,16 +7,16 @@ app = Flask(__name__)
 def hello():
     return "Hello, World!"
 
-def app_handler(environ, start_response):
-    return app(environ, start_response)
-
-class handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type','text/plain')
-        self.end_headers()
-
-        message = "Hello, World!"
-        self.wfile.write(message.encode())
-        return
+# def app_handler(environ, start_response):
+#     return app(environ, start_response)
+# 
+# class handler(BaseHTTPRequestHandler):
+#     def do_GET(self):
+#         self.send_response(200)
+#         self.send_header('Content-type','text/plain')
+#         self.end_headers()
+# 
+#         message = "Hello, World!"
+#         self.wfile.write(message.encode())
+#         return
 
